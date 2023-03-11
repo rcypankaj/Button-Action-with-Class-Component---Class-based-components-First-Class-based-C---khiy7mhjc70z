7585cb9278ce5881f5d0232a185c166e68ceff7b
Pankaj <rcypankaj@gmail.com>
2023-03-11 10:30:25 +0530

    gra

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,32 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    constructor(props) {
-		super(props);
-	};
+  constructor(props) {
+    super(props);
+    this.state = { show: false };
+  }
 
-    render() {
-    	return(
-    		<div id="main">
-				{ /* Do not remove this main div!! */ }
-    		</div>
-    	);
-    }
+  renderParagraphHandler() {
+    this.setState({ show: true });
+  }
+
+  render() {
+    return (
+      <div id="main">
+        {/* Do not remove this main div!! */}
+        {this.state.show && (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
+        <button id="click" onClick={this.renderParagraphHandler.bind(this)}>
+          Click
+        </button>
+      </div>
+    );
+  }
 }
 
-
 export default App;
-
